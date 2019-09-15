6abe53dc9e46fe805e318bb57e2683f7b662f251
alexsemler <alex.semler03@gmail.com>
2019-09-15 17:33:54 -0500

    Moving Mech

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -1,1 +1,15 @@
-let room1 = 5
+let x = 1
+let y = 1
+
+let direction = prompt("Which direction do you want to move (north, east, south, or west)")
+let units = Number(prompt("How many units do you want to move?"))
+
+if (direction == 'north') {
+  let y = (y + units)
+} else if (direction == 'east' {
+  let x = (x + units)
+} else if (direction == 'south' {
+  let y = (y + units)
+} else if (direction == 'west' {
+  let x = (x + units)
+  {
