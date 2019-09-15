79824a59fd2c453092962c10ca93deee15fbf683
Lauren <laurenermer@hotmail.com>
2019-09-15 18:58:12 -0500

    update2

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -1,6 +1,7 @@
 let bedroomCount= 1
 let x = 1
 let y = 1
+var roomArray= []
 
 let direction = prompt("Welcome to the living room! Which direction do you want to move (north, east, south, or west)")
 let units = Number(prompt("How many units do you want to move?"))
@@ -15,42 +16,67 @@ if (direction == 'north') {
   x = (x - units)
 }
   if (x < 0 || x > 15) {
-alert("oopsies, you went too far and ran into the wall!")
+alert("Oopsies, you went too far and ran into the wall!")
 x = (x - units)
 }
 
 //this makes sure that the player remains within the eastern and western walls
 if (y < 0 || y > 15) {
-alert("oopsies, you went too far and ran into the wall!")
+alert("Oopsies, you went too far and ran into the wall!")
 y = (y - units)
 }
 
 switch (bedroomCount) {
 
-  case 6:
+  case 6: alert('Congratulations! You have completed the tour of the house!') 
   
   default : 
   
 if (x > 10 && x < 15 && y > 10 && y < 20) {
   alert('Welcome to the Master Bedroom.')
-  bedroomCount = bedroomCount + 1 
+  roomArray.includes('Master Bedroom')
+  if ('true') {alert('You have already been here')}
+  else if ('false') {
+  bedroomCount = bedroomCount + 1
+  roomArray.push('Master Bedroom')}
+  
 }else if (x > 5 && x < 10 && y > 10 && y < 20) {
   alert('Welcome to the Bathroom.')
-  bedroomCount = bedroomCount + 1 
+  roomArray.includes('Bathroom')
+  if ('true') {alert('You have already been here')}
+  else if ('false') {
+  bedroomCount = bedroomCount + 1
+  roomArray.push('Bathroom')} 
 }else if (x > 0 && x < 5 && y > 10 && y < 20) {
   alert('Welcome to the Closet.')
-  bedroomCount = bedroomCount + 1 
+  if roomArray.includes('Closet') {alert('You have already been here')}
+  else {
+  bedroomCount = bedroomCount + 1
+  roomArray.push('Closet')} 
+  
 }else if (x > 0 && x < 5 && y > 0 && y < 10) {
-  alert('Welcome to the living room! this is where we gather to watch TV');
-  bedroomCount= bedroomCount + 1;}
+  alert('Welcome to the Living Room! this is where we gather to watch TV');
+  roomArray.includes('Living Room')
+  if ('true') {alert('You have already been here')}
+  else if ('false') {
+  bedroomCount = bedroomCount + 1
+  roomArray.push('Living Room')} 
 //coordinates for the kitchen
 else if (x > 5 && x < 10 && y > 0 && y < 10) {
-  alert('Welcome to the kitchen! This is where we cook and eat.');
-  bedroomCount= bedroomCount + 1;}
+  alert('Welcome to the Kitchen! This is where we cook and eat.');
+roomArray.includes('Kitchen')
+  if ('true') {alert('You have already been here')}
+  else if ('false') {
+  bedroomCount = bedroomCount + 1
+  roomArray.push('Kitchen')} 
   //coordinates for the game room
 else if (x > 10 && x < 15 && y > 0 && y < 10) {
-  alert('Welcome to the game room! This is where we like to hang out');
-  bedroomCount= bedroomCount + 1;}
+  alert('Welcome to the Game Room! This is where we like to hang out');
+ roomArray.includes('Game Room')
+  if ('true') {alert('You have already been here')}
+  else if ('false') {
+  bedroomCount = bedroomCount + 1
+  roomArray.push('Game Room')} 
 
 }
 
