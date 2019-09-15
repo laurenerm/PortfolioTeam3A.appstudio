674d5cf845bbf12b1bdb66391b64445d3eb84d24
Lauren <laurenermer@hotmail.com>
2019-09-15 17:33:01 -0500

    jk

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -1,6 +1,5 @@
 alert('Welcome! Would you like to play The Big Adventure?')
-  if (true) {
-  alert('Yay! move up one unit to enter the house')}; 
+  if ('true'){alert('Welcome to the living room')}; 
   
   direction= prompt('Which direction would you like to move? hint: enter up, down, left, or right)');
   unit= prompt(`how many units ${direction} would you like to go?`)
