48873bdab81a59890ec45953a2d9ec5b91071d4c
Lauren <laurenermer@hotmail.com>
2019-09-15 18:28:22 -0500

    update

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -1,6 +1,60 @@
-alert('Welcome! Would you like to play The Big Adventure?')
-  if ('true'){alert('Welcome to the living room')}; 
+let bedroomCount= 1
+let x = 1
+let y = 1
+
+let direction = prompt("Welcome to the living room! Which direction do you want to move (north, east, south, or west)")
+let units = Number(prompt("How many units do you want to move?"))
+
+if (direction == 'north') {
+   y = (y + units)
+} else if (direction == 'east') {
+   x = (x + units)
+} else if (direction == 'south') {
+  y = (y - units)
+} else if (direction == 'west'){
+  x = (x - units)
+}
+  if (x < 0 || x > 15) {
+alert("oopsies, you went too far and ran into the wall!")
+x = (x - units)
+}
+
+//this makes sure that the player remains within the eastern and western walls
+if (y < 0 || y > 15) {
+alert("oopsies, you went too far and ran into the wall!")
+y = (y - units)
+}
+
+switch (bedroomCount) {
+
+  case 6:
   
-  direction= prompt('Which direction would you like to move? hint: enter up, down, left, or right)');
-  unit= prompt(`how many units ${direction} would you like to go?`)
+  default : 
+  
+if (x > 10 && x < 15 && y > 10 && y < 20) {
+  alert('Welcome to the Master Bedroom.')
+  bedroomCount = bedroomCount + 1 
+}else if (x > 5 && x < 10 && y > 10 && y < 20) {
+  alert('Welcome to the Bathroom.')
+  bedroomCount = bedroomCount + 1 
+}else if (x > 0 && x < 5 && y > 10 && y < 20) {
+  alert('Welcome to the Closet.')
+  bedroomCount = bedroomCount + 1 
+}else if (x > 0 && x < 5 && y > 0 && y < 10) {
+  alert('Welcome to the living room! this is where we gather to watch TV');
+  bedroomCount= bedroomCount + 1;}
+//coordinates for the kitchen
+else if (x > 5 && x < 10 && y > 0 && y < 10) {
+  alert('Welcome to the kitchen! This is where we cook and eat.');
+  bedroomCount= bedroomCount + 1;}
+  //coordinates for the game room
+else if (x > 10 && x < 15 && y > 0 && y < 10) {
+  alert('Welcome to the game room! This is where we like to hang out');
+  bedroomCount= bedroomCount + 1;}
+
+}
+
+
+
+  
   
